13a105d5c3c1752532d5ba72b4a493437b6fe377
Maryia Ondra <ya.ondra@yandex.by>
2021-02-20 12:21:13 +0300

    added skybox

--- Assets/Themes/1.1 SolarSystem/Scripts/CameraController.cs
+++ Assets/Themes/1.1 SolarSystem/Scripts/CameraController.cs
@@ -4,9 +4,19 @@ namespace UnityBase.SolarSystem
 {
     public class CameraController : MonoBehaviour
     {
+        private float speed = 50;
+
         private void Awake()
         {
             Cursor.visible = false;
+        }
+
+        private void Update()
+        {
+            float _horizontal = Input.GetAxis("Horizontal");
+            float _vertical = Input.GetAxis("Vertical");
+
+            transform.position += new Vector3(_horizontal, 0, _vertical) * speed * Time.deltaTime;
         }
     }
 }
