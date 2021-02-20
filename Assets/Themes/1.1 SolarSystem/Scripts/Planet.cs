5f2e32fd715f97e9241ee692b9bd8e6eda0d45ab
Maryia Ondra <ya.ondra@yandex.by>
2021-02-20 13:04:08 +0300

    fixed all bugs

--- Assets/Themes/1.1 SolarSystem/Scripts/Planet.cs
+++ Assets/Themes/1.1 SolarSystem/Scripts/Planet.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using DG.Tweening;
+using System;
+using UnityEngine;
 
 namespace UnityBase.SolarSystem
 {
@@ -7,22 +9,53 @@ namespace UnityBase.SolarSystem
         [SerializeField] private float _selfRotationSpeed;
         [SerializeField] private float _planetSpaceSpeed;
         [SerializeField] private GameObject _rotationCenter;
+        [SerializeField] private int segments = 2000;
 
         private float _scaleFactor = 2;
 
         [SerializeField] float _minEllipseRadius;
         [SerializeField] float _maxEllipseRadius;
-        private float _angle;
+
+
+        LineRenderer lineRenderer;
+        Vector3[] pathArray;
+
 
         private void Awake()
         {
             transform.localScale = transform.localScale * _scaleFactor;
+            lineRenderer = GetComponent<LineRenderer>();
+            lineRenderer.positionCount = segments + 1;
+            pathArray = new Vector3[segments + 1];
+        }
+
+        private void Start()
+        {
+            transform.position = pathArray[segments];
+            DrowTrajectory();
+            transform.DOPath(pathArray, _planetSpaceSpeed * segments).SetLoops(-1);
+        }
+
+        private void DrowTrajectory()
+        {
+            float angle = 20;
+
+            for (int i = 0; i < (segments + 1); i++)
+            {
+                float xPos = Mathf.Cos(angle) * _minEllipseRadius;
+                float yPos = Mathf.Sin(angle) * _maxEllipseRadius;
+
+                Vector3 _position = new Vector3(xPos, 0, yPos);
+                lineRenderer.SetPosition(i, _position );
+                pathArray[i] = _position;
+                angle += (360f / segments);
+            }
         }
 
         private void Update()
         {
             RotateAroundYourself();
-            RotateLikeEllipse();
+            //RotateLikeEllipse();
         }
 
         private void RotateAroundYourself()
@@ -31,14 +64,14 @@ namespace UnityBase.SolarSystem
         }
 
 
-        private void RotateLikeEllipse()
-        {
-            _angle += _planetSpaceSpeed * Time.deltaTime;
+        //private void RotateLikeEllipse()
+        //{
+        //    angle += _planetSpaceSpeed * Time.deltaTime;
 
-            float xPos = Mathf.Cos(_angle) * _minEllipseRadius;
-            float yPos = Mathf.Sin(_angle) * _maxEllipseRadius;
+        //    float xPos = Mathf.Cos(angle) * _minEllipseRadius;
+        //    float yPos = Mathf.Sin(angle) * _maxEllipseRadius;
 
-            transform.position = new Vector3(xPos, 0, yPos);
-        }
+        //    transform.position = new Vector3(xPos, 0, yPos);
+        //}
     }
 }
